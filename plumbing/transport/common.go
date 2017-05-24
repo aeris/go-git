69ddef0ce5d218d41bf18fee6a76dcc39a489999
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2017-05-24 14:11:21 +0200

    Do not use go1.8 methods

--- plumbing/transport/common.go
+++ plumbing/transport/common.go
@@ -19,6 +19,7 @@ import (
 	"net/url"
 	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/cozy/go-git/plumbing"
 	"github.com/cozy/go-git/plumbing/protocol/packp"
@@ -137,7 +138,7 @@ type urlEndpoint struct {
 }
 
 func (e urlEndpoint) Protocol() string { return e.URL.Scheme }
-func (e urlEndpoint) Host() string     { return e.URL.Hostname() }
+func (e urlEndpoint) Host() string     { return stripPort(e.URL.Host) }
 
 func (e urlEndpoint) User() string {
 	if e.URL.User == nil {
@@ -157,12 +158,12 @@ func (e urlEndpoint) Password() string {
 }
 
 func (e urlEndpoint) Port() int {
-	p := e.URL.Port()
+	p := portOnly(e.URL.Host)
 	if p == "" {
 		return 0
 	}
 
-	i, err := strconv.Atoi(e.URL.Port())
+	i, err := strconv.Atoi(portOnly(e.URL.Host))
 	if err != nil {
 		return 0
 	}
@@ -259,4 +260,29 @@ func FilterUnsupportedCapabilities(list *capability.List) {
 	for _, c := range UnsupportedCapabilities {
 		list.Delete(c)
 	}
+}
+
+func stripPort(hostport string) string {
+	colon := strings.IndexByte(hostport, ':')
+	if colon == -1 {
+		return hostport
+	}
+	if i := strings.IndexByte(hostport, ']'); i != -1 {
+		return strings.TrimPrefix(hostport[:i], "[")
+	}
+	return hostport[:colon]
+}
+
+func portOnly(hostport string) string {
+	colon := strings.IndexByte(hostport, ':')
+	if colon == -1 {
+		return ""
+	}
+	if i := strings.Index(hostport, "]:"); i != -1 {
+		return hostport[i+len("]:"):]
+	}
+	if strings.Contains(hostport, "]") {
+		return ""
+	}
+	return hostport[colon+len(":"):]
 }
